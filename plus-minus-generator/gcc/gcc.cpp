5006e545b7acd15aaf0b8a4fee61f963668690bb
warnov <warnov@gmail.com>
2023-01-11 14:19:40 -0500

    added c++  solution

---
+++ plus-minus-generator/gcc/gcc.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+char a[100];
+
+int main()
+{
+	int cursor, n;
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d", &n);
+		scanf("%s", a);
+		int total = (int)(a[0] - '0');
+		cursor = 0;
+		for (int i = 1; i < n; i++) {
+			int current = (int)(a[i] - '0');
+			if (total == 0)printf("%c", '+');
+			else printf("%c", '-');
+			total ^= current;
+		}
+		printf("%c", '\n');
+	}
+	return 0;
+}
