bbc8a0122bf7a0de5ee46b942fe8d4b57db8c0be
warnov <warnov@gmail.com>
2023-01-12 03:05:06 -0500

    odd divisor added

---
+++ odd-divisor/csharp/odd-divisor/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+var t = int.Parse(Console.ReadLine());
+while (t-- > 0)
+{
+    var n = Int64.Parse(Console.ReadLine());
+    if ((n & (n - 1)) != 0) sb.AppendLine("YES");
+    else sb.AppendLine("NO");
+}
+Console.Write(sb.ToString());
---
+++ odd-divisor/gcc/gcc.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<math.h>
+
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        long long n;
+        long double  m;
+        scanf("%lld", &n);
+        m = log2l(n);
+        if (m == floorl(m)) {
+            printf("NO\n");
+        }
+        else {
+            printf("YES\n");
+        }
+    }
+}
+
+
+
